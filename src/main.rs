40574666749a9ec6e68bf3626580b46a553fd724
WinsomeQuill <artemqa.belyakov07@gmail.com>
2021-10-26 12:27:16 +0700

    Small update

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,12 @@ fn add_attempts() {
     }
 }
 
+fn clear_attempts() {
+    unsafe {
+        ATTEMPTS = 0;
+    }
+}
+
 fn get_number() -> u32 {
     unsafe {
         return NUMBER;
@@ -30,15 +36,19 @@ fn generate_number() {
     }
 }
 
+fn read_line() -> String {
+    let mut input: String = String::new();
+    io::stdin().read_line(&mut input);
+    return input;
+}
+
 fn main() {
+    let mut run: bool = true;
     generate_number();
-    loop {
+    while run {
         println!("Write any number of 1 to 10: ");
-        let mut input: String = String::new();
         
-        io::stdin().read_line(&mut input)
-            .expect("Error, invalid number!");
-    
+        let input = read_line();
         let input: u32 = match input.trim().parse() {
             Ok(num) => num,
             Err(_) => {
@@ -46,11 +56,29 @@ fn main() {
                 continue;
             },
         };
+        
         add_attempts();
 
         if input == get_number() {
             println!("Congratulations! You are win!\nNumber: {}\nTotal attempts: {}", input, get_attempts());
-            break;
+            println!("Maybe restart game? [Y/n]");
+            loop {
+                let mut input: String = read_line();
+                input = input.trim().to_lowercase();
+                if input != "n" && input != "y" {
+                    println!("Error, use Y or N!");
+                    continue;
+                }
+
+                if input == "n" {
+                    run = false;
+                    break;
+                } else {
+                    clear_attempts();
+                    generate_number();
+                    break;
+                }
+            }
         }
         
         if input != get_number() {
