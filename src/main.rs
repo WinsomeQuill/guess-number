d2237a294c4dc78f2d4b070e801312e1e17bc627
WinsomeQuill <Artemqa.belyakov07@gmail.com>
2021-11-21 01:34:39 +0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,88 +1,27 @@
-extern crate rand;
-
-use std::io;
+use std::io::stdin;
 use rand::Rng;
 
-static mut NUMBER: u32 = 0;
-static mut ATTEMPTS: u32 = 0;
-
-fn get_attempts() -> u32 {
-    unsafe {
-        return ATTEMPTS;
-    }
-}
-
-fn add_attempts() {
-    unsafe {
-        ATTEMPTS += 1;
-    }
-}
-
-fn clear_attempts() {
-    unsafe {
-        ATTEMPTS = 0;
-    }
-}
-
-fn get_number() -> u32 {
-    unsafe {
-        return NUMBER;
-    }
-}
-
-fn generate_number() {
-    unsafe {
-        NUMBER = rand::thread_rng().gen_range(1, 10);
-    }
-}
-
-fn read_line() -> String {
-    let mut input: String = String::new();
-    io::stdin().read_line(&mut input);
-    return input;
+fn read_line() -> u32 {
+    let mut buf = String::new();
+    stdin().read_line(&mut buf);
+    return match buf.trim_end().parse::<u32>() {
+        Ok(i) => i,
+        Err(_) => 0,
+    };
 }
 
 fn main() {
-    let mut run: bool = true;
-    generate_number();
-    while run {
-        println!("Write any number of 1 to 10: ");
+    let mut attempts: u32 = 0;
+    let number: u32 = rand::thread_rng().gen_range(1, 10);
+    loop {
         
-        let input = read_line();
-        let input: u32 = match input.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {
-                println!("Error, invalid number!");
-                continue;
-            },
+        let line = read_line();
+        let res = match line {
+            0 => { println!("Invalid number!"); continue; },
+            _ => line
         };
-        
-        add_attempts();
 
-        if input == get_number() {
-            println!("Congratulations! You are win!\nNumber: {}\nTotal attempts: {}", input, get_attempts());
-            println!("Maybe restart game? [Y/n]");
-            loop {
-                let mut input: String = read_line();
-                input = input.trim().to_lowercase();
-                if input != "n" && input != "y" {
-                    println!("Error, use Y or N!");
-                    continue;
-                }
-
-                if input == "n" {
-                    run = false;
-                    break;
-                } else {
-                    clear_attempts();
-                    generate_number();
-                    break;
-                }
-            }
-        }
-        
-        if input != get_number() {
-            println!("No, this is not my number!");
-        }
+        if number == res { println!("Congratulations! Number: {} | Attempts: {}", number, attempts); break; }
+        println!("It's not my number!"); attempts += 1;
     }
 }
